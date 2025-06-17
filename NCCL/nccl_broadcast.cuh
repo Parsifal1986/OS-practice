4dba9c9bbb4ec050c3320a2716a93f8b2bbfdddc
Chuxi Wu <parsifal1986@sjtu.edu.cn>
2025-06-17 14:51:17 +0800

    Update the nccl broadcast practice.

---
+++ NCCL/nccl_broadcast.cuh
@@ -0,0 +1,38 @@
+#ifndef NCCL_BROADCAST_CUH
+#define NCCL_BROADCAST_CUH
+
+#define CUDACHECK(cmd) do {                         \
+  cudaError_t err = cmd;                            \
+  if (err != cudaSuccess) {                         \
+    printf("Failed: Cuda error %s:%d '%s'\n",       \
+        __FILE__,__LINE__,cudaGetErrorString(err)); \
+    exit(EXIT_FAILURE);                             \
+  }                                                 \
+} while(0)
+
+#define NCCLCHECK(cmd) do {                         \
+  ncclResult_t res = cmd;                           \
+  if (res != ncclSuccess) {                         \
+    printf("Failed, NCCL error %s:%d '%s'\n",       \
+        __FILE__,__LINE__,ncclGetErrorString(res)); \
+    exit(EXIT_FAILURE);                             \
+  }                                                 \
+} while(0)
+
+#include <nccl.h>
+#include <cuda_runtime.h>
+
+ncclResult_t nccl_broadcast_data(void* data, size_t count, int root, ncclComm_t comm) {
+  cudaStream_t stream;
+  CUDACHECK(cudaStreamCreate(&stream));
+
+  float* d_ptr;
+  CUDACHECK(cudaMalloc((void **)&d_ptr, count * sizeof(float)));
+  CUDACHECK(cudaMemcpyAsync(d_ptr, data, count * sizeof(float), cudaMemcpyHostToDevice, stream));
+  NCCLCHECK(ncclBroadcast(d_ptr, d_ptr, count, ncclFloat, root, comm, stream));
+  CUDACHECK(cudaStreamSynchronize(stream));
+  CUDACHECK(cudaFree(d_ptr));
+  return ncclSuccess;
+}
+
+#endif // NCCL_BROADCAST_CUH
